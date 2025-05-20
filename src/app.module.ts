5698ea50a4d739d62b1459cc60d71346a76340ee
Igor Nascimento <64155074+IgorNCS@users.noreply.github.com>
2025-05-19 22:37:08 -0300

    Iniciado configuração de cacheamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppDataSource } from './database/data-source';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ClsModule } from 'nestjs-cls';
+import { CacheModule } from '@nestjs/cache-manager';
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: async () => {
         await AppDataSource.initialize();
@@ -14,6 +21,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       dataSourceFactory: async (options) => {
         return AppDataSource;
       },
+    }),
+    CacheModule.register(),
+    ClsModule.forRoot({
+      global: true,
+      middleware: { mount: true },
+      interceptor: { mount: false },
     }),
   ],
   controllers: [AppController],
