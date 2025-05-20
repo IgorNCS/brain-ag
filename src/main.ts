4a1b1fb2092258b5b1489f8e9811d25956195744
Igor Nascimento <64155074+IgorNCS@users.noreply.github.com>
2025-05-19 22:34:20 -0300

    Configurado Swagger para documentação.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Brain Agriculture')
+    .setDescription('Documentação do Brain Agriculture')
+    .setContact(
+      'Igor Nascimento da Costa Silva',
+      'https://github.com/IgorNCS',
+      'igor.ncsilva@hotmail.com',
+    )
+    .setVersion('1.0')
+    .addTag('pix-automatico')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation/api', app, document);
+
+
+  writeFileSync('./swagger-spec.json', JSON.stringify(document, null, 2));
+  console.log('Documentação Swagger JSON gerada em swagger-spec.json');
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation/api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
