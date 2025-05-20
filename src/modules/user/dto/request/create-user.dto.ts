9348cf09988ac2a0d1a524aae8d18b04d029d868
Igor Nascimento <64155074+IgorNCS@users.noreply.github.com>
2025-05-20 19:35:16 -0300

    Adicionado validação de CpfCnpj

--- src/modules/user/dto/request/create-user.dto.ts
+++ src/modules/user/dto/request/create-user.dto.ts
@@ -1,47 +1,49 @@
 import {
-    IsOptional,
-    IsNumber,
-    Min,
-    IsDateString,
-    IsString,
-    MaxLength,
-    Max,
-    IsNotEmpty,
-    IsEnum,
-  } from 'class-validator';
-  import { Transform } from 'class-transformer';
+  IsOptional,
+  IsNumber,
+  Min,
+  IsDateString,
+  IsString,
+  MaxLength,
+  Max,
+  IsNotEmpty,
+  IsEnum,
+  ValidateIf,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Role } from '../../entities/user.entity';
+import { cpf, cnpj } from 'cpf-cnpj-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ example: 'Fulano de tal' })
+  name: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @ApiProperty({ example: 'Fulano de tal' })
-    name: string;
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ example: 'fulano@gmail.com' })
+  email: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @ApiProperty({ example: 'fulano@gmail.com' })
-    email: string;
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ example: 'senhasecreta123' })
+  password: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @ApiProperty({ example: 'senhasecreta123' })
-    password: string;
+  @IsOptional()
+  @IsEnum(Role)
+  @ApiPropertyOptional({
+    example: 'costumer',
+    description: 'Apenas ADMIN pode enviar',
+  })
+  role?: Role;
 
-    @IsOptional()
-    @IsEnum(Role)
-    @ApiPropertyOptional({ example: 'costumer',description: 'Apenas ADMIN pode enviar' })
-    role?: Role;
-
-    @IsNotEmpty()
-    @IsNumber()
-    @ApiProperty({ example: '12345678900' })
-    cpfCnpj: number;
-  
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty({ example: '12345678900' })
+  @ValidateIf((o) =>
+    o.cpfCnpj.length <= 11 ? cpf.isValid(o.cpfCnpj) : cnpj.isValid(o.cpfCnpj),
+  )
+  cpfCnpj: number;
 }
-
-
-
-  
